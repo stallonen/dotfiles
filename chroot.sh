762aa65150eabf2e0c18eafc970dcc25537d84fb
stallonen <neelroboinfo365@gmail.com>
2020-02-27 23:15:37 -0500

    added chroot.sh for gentoo live cd purposes

---
+++ chroot.sh
@@ -0,0 +1,10 @@
+mount /dev/sdd3 /mnt/gentoo
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev 
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+mount /dev/sdd2 /boot
