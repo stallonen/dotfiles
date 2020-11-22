d341fbe5863edda7ceccff8bc6f4d59dabfd2d5b
Neel <neelroboinfo365@gmail.com>
2020-11-23 00:28:32 +0530

    modified:   bspwm/.config/bspwm/bspwmrc 	modified:   dmenu/.dmenu/power.sh 	modified:   polybar/.config/polybar/bspwm-polbar.conf 	modified:   ranger/.config/ranger/plugins/__pycache__/__init__.cpython-37.opt-1.pyc 	modified:   ranger/.config/ranger/plugins/__pycache__/cmus.cpython-37.opt-1.pyc 	new file:   rofi/.config/rofi/awel.rasi 	modified:   rofi/.config/rofi/config.rasi 	new file:   rofi/.config/rofi/config.rasi.back 	modified:   sxhkd/.config/sxhkd/sxhkdrc 	modified:   vim/.vim/.netrwhist 	modified:   x/.xinitrc

--- dmenu/.dmenu/power.sh
+++ dmenu/.dmenu/power.sh
@@ -1,5 +1,6 @@
 declare -a options=("shutdown
 restart
+suspend
 quit")
 . "${HOME}/.cache/wal/colors.sh"
 choice=$(echo -e "${options[@]}" | dmenu  -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "Iosevka:12" -i -p  'Whatcha want to do ?')
@@ -8,9 +9,14 @@ choice=$(echo -e "${options[@]}" | dmenu  -nb "$color0" -nf "$color15" -sb "$col
 		echo "Program terminated."
 	fi
 	if [ "$choice" == 'shutdown' ]; then
-	exec dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
+	loginctl poweroff
 	fi
 	if [ "$choice" == 'restart' ]; then
-        exec dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
+	loginctl reboot
 	fi
+	if [ "$choice" == 'suspend' ]; then
+	echo "XHC" >/proc/acpi/wakeup
+	loginctl suspend
+	fi
+
 
